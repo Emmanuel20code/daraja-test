a0746714b2c42e720ea8116fde5656e8673eabeb
Emmanuel20code <emmanueloyaro3@gmail.com>
2026-07-24 20:33:45 +0300

    Remove duplicate Pool import from server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
 const express = require("express");
+const { Pool } = require("pg");
 const axios = require("axios");
 const moment = require("moment");
-const { Pool } = require("pg");
+
 
 const app = express();
 
